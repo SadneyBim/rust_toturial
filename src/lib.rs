2143f9cadb63d4608529e52a6cb8d7d047ec4a79
sadneyBim <urarintaro0@gmail.com>
2025-06-02 18:46:12 -0700

    add lib

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,21 @@ mod front_of_house {
     }
 }
 
+pub use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
     // Change our mind about what bread we'd like.
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
+
+
 
 fn deliver_orer() {}
 
@@ -44,5 +52,10 @@ mod back_of_house {
                 seasonal_fruit: String::from("peaches")
             }
         }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad
     }
 }
